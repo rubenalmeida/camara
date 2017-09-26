3c60bb2a534aa69cfea3ad7794f0ad5b915531cb
kurtye <kurtyebsb@gmail.com>
2017-09-26 11:33:58 -0300

    Principal

--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ServiceProvider } from "../../providers/service-provider";
-import {jsonpFactory} from "@angular/http/src/http_module";
+import { DeputadosPage } from '../deputados/deputados';
+import { PerfilDeputadoPage } from '../perfil-deputado/perfil-deputado';
+import { PartidosPage } from '../partidos/partidos';
+import { PerfilPartidoPage } from '../perfil-partido/perfil-partido';
+import { LegislaturaPage } from '../legislatura/legislatura';
+import { BlocosPage } from '../blocos/blocos';
+import { OrgaosPage } from '../orgaos/orgaos';
+import { FrentesPage } from '../frentes/frentes';
+import { EventosPage } from '../eventos/eventos';
+import { ProposicoesPage } from '../proposicoes/proposicoes';
+import { VotacoesPage } from '../votacoes/votacoes';
+import { ReferenciasPage } from '../referencias/referencias';
+import { AtualizacoesPage } from '../atualizacoes/atualizacoes';
 
 @Component({
   selector: 'page-principal',
@@ -9,17 +20,46 @@ import {jsonpFactory} from "@angular/http/src/http_module";
 })
 export class PrincipalPage {
 
-   partidos: any[];
-
-  constructor(public navCtrl: NavController, public service: ServiceProvider) {
+  constructor(public navCtrl: NavController) {
   }
-
-  ionViewDidLoad(){
-    this.service.getPartidos().subscribe(data =>{
-      console.log(data.dados);
-      this.partidos = data.dados;
-    });
+  goToDeputados(params){
+    if (!params) params = {};
+    this.navCtrl.push(DeputadosPage);
+  }goToPerfilDeputado(params){
+    if (!params) params = {};
+    this.navCtrl.push(PerfilDeputadoPage);
+  }goToPartidos(params){
+    if (!params) params = {};
+    this.navCtrl.push(PartidosPage);
+  }goToPerfilPartido(params){
+    if (!params) params = {};
+    this.navCtrl.push(PerfilPartidoPage);
+  }goToLegislatura(params){
+    if (!params) params = {};
+    this.navCtrl.push(LegislaturaPage);
+  }goToBlocos(params){
+    if (!params) params = {};
+    this.navCtrl.push(BlocosPage);
+  }goToOrgaos(params){
+    if (!params) params = {};
+    this.navCtrl.push(OrgaosPage);
+  }goToFrentes(params){
+    if (!params) params = {};
+    this.navCtrl.push(FrentesPage);
+  }goToEventos(params){
+    if (!params) params = {};
+    this.navCtrl.push(EventosPage);
+  }goToProposicoes(params){
+    if (!params) params = {};
+    this.navCtrl.push(ProposicoesPage);
+  }goToVotacoes(params){
+    if (!params) params = {};
+    this.navCtrl.push(VotacoesPage);
+  }goToReferencias(params){
+    if (!params) params = {};
+    this.navCtrl.push(ReferenciasPage);
+  }goToAtualizacoes(params){
+    if (!params) params = {};
+    this.navCtrl.push(AtualizacoesPage);
   }
-
-
 }
