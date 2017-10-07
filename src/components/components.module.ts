cde103a6ea7249d6c1e745885a1a3567586653fa
marlon <kurtyebsb@gmail.com>
2017-10-07 18:44:47 -0300

    Att Layout CSS, perfilDeputado e Partido

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { DespesasDeputadoComponent } from './despesas-deputado/despesas-deputado';
 import { ProposicoesDeputadoComponent } from './proposicoes-deputado/proposicoes-deputado';
+import { ExpandableComponent } from './expandable/expandable';
 @NgModule({
 	declarations: [DespesasDeputadoComponent,
-    ProposicoesDeputadoComponent],
+    ProposicoesDeputadoComponent,
+    ExpandableComponent],
 	imports: [],
 	exports: [DespesasDeputadoComponent,
-    ProposicoesDeputadoComponent]
+    ProposicoesDeputadoComponent,
+    ExpandableComponent]
 })
 export class ComponentsModule {}
